733687d1f89499efc475b0d9ebba4e0a5d094daf
aashish <aashish@ubuntu>
2020-03-24 10:55:53 +0545

    Created a collection of task in database

--- mongodb.js
+++ mongodb.js
@@ -7,15 +7,32 @@ const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, {useUnifiedTopology: true}, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, { useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log("Client configuration failed!!!");
     }
     
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Aashish',
-        age: 22
+    //inserts many documents into single collection of database
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Create weather application',
+            completed: true
+        },
+        {
+            description: 'Learn mongodb',
+            completed: false
+        },
+        {
+            description: 'Learn promises and asynchronous function',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log("Unable to insert task");
+        }
+        console.log(result.ops);
     })
 })
