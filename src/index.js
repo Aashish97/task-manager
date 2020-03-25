d0d88706908448039f46a7a8170f67d9599aea3b
aashish <aashish@ubuntu>
2020-03-25 10:39:13 +0545

    Added get request for the tasks endpoint

--- src/index.js
+++ src/index.js
@@ -29,8 +29,14 @@ app.get('/users/:id', (req, res) => {
 
     const _id = req.params.id;
     User.findById(_id)
-    .then( user => res.send(user))
-    .catch(() => res.status(404).send())
+    .then( user => {
+        if(!user){
+            return res.status(404).send();
+        }
+        
+        res.send(user)
+    })
+    .catch(() => res.status(404).send());
 })
 
 app.post('/tasks', (req, res) => {
@@ -39,6 +45,25 @@ app.post('/tasks', (req, res) => {
     task.save()
     .then(() => res.status(201).send(task))
     .catch( e => res.status(400).send(e))
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({})
+    .then(tasks => res.send(tasks))
+    .catch(() => res.status(500).send());
+});
+
+app.get('/tasks/:id', (req, res) => {
+
+    const _id = req.params.id;
+    Task.findById(_id)
+    .then( task => {
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task)
+    })
+    .catch(() => res.status(404).send())
 })
 
 app.listen(port, () => {
