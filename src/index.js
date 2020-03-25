a49e347e95d4ebae20dc336603a1c193cdc1d3e4
aashish <aashish@ubuntu>
2020-03-25 13:59:58 +0545

    Used async/await instead of promise chaining

--- src/index.js
+++ src/index.js
@@ -10,33 +10,41 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
 
-    user.save()
-    .then(()=> res.status(201).send(user))
-    .catch( e => res.status(400).send(e));
+    try{
+        await user.save();
+        res.status(201).send(user);
+    }catch{
+        res.status(400).send();
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find({})
-    .then(users => res.send(users))
-    .catch(() => res.status(500).send());
+app.get('/users', async (req, res) => {
+    
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch{
+        res.status(500).send();
+    }
 });
 
 // :id provides the dynamic content that the users will provide as user_id
-app.get('/users/:id', (req, res) => {
-
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
-    User.findById(_id)
-    .then( user => {
-        if(!user){
+
+    try{
+        const user = await User.findById(_id);
+        if (!user) {
             return res.status(404).send();
         }
         
         res.send(user)
-    })
-    .catch(() => res.status(404).send());
+    }catch{
+        res.status(404).send();
+    }
 })
 
 app.post('/tasks', (req, res) => {
