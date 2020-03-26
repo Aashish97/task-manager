b1248dfc01e5deb729fdb3da5dfe0a1af7f0ba0c
aashish <aashish@ubuntu>
2020-03-26 16:35:54 +0545

    Code refactorization and timestamp for creadtedDate and updateDate added

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema(
+  {
     description: {
       type: String,
       required: true,
@@ -8,12 +9,18 @@ const Task = mongoose.model('Task', {
     },
     completed: {
       type: Boolean,
-      default: false,
+      default: false
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       required: true
     }
-});
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
