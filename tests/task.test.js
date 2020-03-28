a2c22cb810575a81333f77aff3f02bd2d6f831d6
aashish <aashish@ubuntu>
2020-03-28 23:03:47 +0545

    Added different task test cases

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,19 +1,41 @@
-const request = require('supertest');
+const request = require("supertest");
 const app = require("./../src/app");
-const Task = require('./../src/models/task');
-const {userOneId, userOne, setupDatabase} = require('./fixtures/db')
+const Task = require("./../src/models/task");
+const { userOneId, userOne, userTwoId, userTwo, taskOne, taskTwo, taskThree, setupDatabase } = require("./fixtures/db");
 
 beforeEach(setupDatabase);
 
-test('Should create task for user', async () => {
-    const response = await request(app).post('/tasks')
+test("Should create task for user", async () => {
+  const response = await request(app)
+    .post("/tasks")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send({
-        description: 'From the test'
+      description: "From the test"
     })
-    .expect(201)
+    .expect(201);
 
-    const task = await Task.findById(response.body._id);
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toEqual(false);
+});
+
+test("Should fetch user tasks", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test("Should not be able to delete other user's task", async () => {
+    const response = await request(app)
+    .delete(`/tasks/${userOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+ 
+  const task = await Task.findById(taskOne._id);
     expect(task).not.toBeNull();
-    expect(task.completed).toEqual(false);
 });
